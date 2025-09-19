e4c6782dd82d23dadaf1c93a875f1891d728d345
twwong88 <152827464+twwong88@users.noreply.github.com>
2025-09-19 11:33:39 +0800

    yes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fetch = (...args) => import('node-fetch').then(({ default: f }) => f(...args));
 
 app.get('/locust-stats', async (req, res) => {
-    const r = await fetch('http://57.158.141.77:8089/stats/requests');
+    const r = await fetch('http://4.144.183.18:8089/stats/requests');
     const json = await r.json();
     res.json({ user_count: json.user_count });
 });
